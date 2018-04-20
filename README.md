6b47c36ac05ac2efb92d348baba33d3a1972c612
malkazwini <malkazwini@gmail.com>
2018-04-20 11:10:52 -0400

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# MSSQL CLient (SQLCMD) in a Docker Ephemeral Container 
+
+The ephemeral container will run to execute a single command to Drop a remote Db, the conrainer expects  environment variables to be provided at run. 
+
+## Build the container
+
+clone the repo and: 
+
+docker build -t mssqlclient:latest . 
+
+
+
+ 
+## Running the container
+docker run -e "DbHost=<DB ENDPOINT>" -e "DbPort=<DB PORT> -e "DbUser=<DB USERNAME>" \
+ 		   -e "DbPassword =<DB PASSWORD>" -e "DbName=<DB NAME>" \ 
+ 		    -e "DbMode=<EITHER DROP/ALTER DB>" -e "ScriptDebug=<true/flase>" \ mssqlclient:latest
+
+
+## Contribution 
+
+Feel free to contribute and add more SQL in db_ops.sh
+
---
+++ db_ops.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+while [[ $# -gt 1 ]]
+do
+    INPUT="$1"
+    case $INPUT in
+        -h|--host)
+            DbHost="$2"
+            shift
+            ;;
+        -p|--port)
+            DbPort="$2"
+            shift
+            ;;
+        -u|--user)
+            DbUser="$2"
+            shift
+            ;;
+        -pwd|--password)
+            DbPassword="$2"
+            shift
+            ;;
+        -db|--database)
+            DbName="$2"
+            shift
+            ;;
+        -m|--mode)
+            # [dropDb|alterDb]
+            DbMode="$2"
+            shift
+            ;;
+        -d|--debug)
+            debug="$2"
+            if [[ $debug == "true" ]]; then set -x; fi
+            shift
+            ;;
+        * )
+            do_help
+            ;;
+    esac
+    shift
+done
+
+do_help(){
+    echo "Please provide --host [host] --port [port#] --user  --debug [true|false] --mode [dropDb|alterDb]"
+}
+
+
+
+drop_database(){
+    /opt/mssql-tools/bin/sqlcmd -E -S tcp:"${DbHHost}","${DbPort}" -U "${DbUser}" -P "${DbPassword}" -Q "DROP DATABASE ${DbName}"
+}
+alterDb(){
+    echo "Work in progreess!"
+}
+
+
+
+case $mode in
+    dropDb )
+        drop_database
+    ;;
+    alterDb )
+        alter_database
+        ;;
+    *)
+        ;;
+esac
+
